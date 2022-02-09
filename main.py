796941b0bed6d35706b89ff5833c082973d343ad
Rogerkoranteng <77711618+rogerkorantenng@users.noreply.github.com>
2022-02-09 21:17:03 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-
-#!/usr/bin/env python
+#The app takes real-time data from usgs website and apply's the machine learning technique(XGBoost) through each loop it makes and can predict to a range of five days
 from flask import Flask
 
 app = Flask(__name__)
+
 from flask import render_template, flash, request
 import logging, io, base64, os, datetime
 from datetime import datetime
 from datetime import timedelta
-import pandas as pd
 import numpy as np
 import xgboost as xgb
-
+import pandas as pd
 
 # Variables(Global)
 earthquake_live = None
@@ -30,54 +29,62 @@ def prepare_earthquake_data_and_model(days_out_to_predict = 5, max_depth=3, eta=
 #Return : Pandas Dataframe (Prediction dataframe with live/ future NaN values in outcome magnitutde of quake that has to be predicted)
     
     # get latest data from USGS servers
-    df = pd.read_csv('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.csv')
-    df = df.sort_values('time', ascending=True)
+
+    data = pd.read_csv('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.csv')
+    data = data.sort_values('time', ascending=True)
     # truncate time from datetime
-    df['date'] = df['time'].str[0:10]
+    data['date'] = data['time'].str[0:10]
 
-    # only keep the columns needed
-    df = df[['date', 'latitude', 'longitude', 'depth', 'mag', 'place']]
-    temp_df = df['place'].str.split(', ', expand=True) 
-    df['place'] = temp_df[1]
-    df = df[['date', 'latitude', 'longitude', 'depth', 'mag', 'place']]
+    # For better performance of the app and model we will keep  only columns needed
 
-    # calculate mean lat lon for simplified locations
-    df_coords = df[['place', 'latitude', 'longitude']]
-    df_coords = df_coords.groupby(['place'], as_index=False).mean()
-    df_coords = df_coords[['place', 'latitude', 'longitude']]
+    data = data[['date', 'latitude', 'longitude', 'depth', 'mag', 'place']]
+    temp_data = data['place'].str.split(', ', expand=True) 
+    data['place'] = temp_data[1]
+    data = data[['date', 'latitude', 'longitude', 'depth', 'mag', 'place']]
 
-    df = df[['date', 'depth', 'mag', 'place']]
-    df = pd.merge(left=df, right=df_coords, how='inner', on=['place'])
+    
+    #Get the mean latitude and longitude 
+	
+    data_coords = data[['place', 'latitude', 'longitude']]
+    data_coords = data_coords.groupby(['place'], as_index=False).mean()
+    data_coords = data_coords[['place', 'latitude', 'longitude']]
+	
+    data = data[['date', 'depth', 'mag', 'place']]
+    data = pd.merge(left=data, right=data_coords, how='inner', on=['place'])
 
     # Apply the machine learning algorithm(XGBoost) to each loop
+    
     eq_data = []
-    df_live = []
-    for symbol in list(set(df['place'])):
-        temp_df = df[df['place'] == symbol].copy()
-        temp_df['depth_avg_22'] = temp_df['depth'].rolling(window=22,center=False).mean() 
-        temp_df['depth_avg_15'] = temp_df['depth'].rolling(window=15,center=False).mean()
-        temp_df['depth_avg_7'] = temp_df['depth'].rolling(window=7,center=False).mean()
-        temp_df['mag_avg_22'] = temp_df['mag'].rolling(window=22,center=False).mean() 
-        temp_df['mag_avg_15'] = temp_df['mag'].rolling(window=15,center=False).mean()
-        temp_df['mag_avg_7'] = temp_df['mag'].rolling(window=7,center=False).mean()
-        temp_df.loc[:, 'mag_outcome'] = temp_df.loc[:, 'mag_avg_7'].shift(days_out_to_predict * -1)
+    data_live = []
+    for symbol in list(set(data['place'])):
+        temp_data = data[data['place'] == symbol].copy()
+        temp_data['depth_avg_22'] = temp_data['depth'].rolling(window=22,center=False).mean() 
+        temp_data['depth_avg_15'] = temp_data['depth'].rolling(window=15,center=False).mean()
+        temp_data['depth_avg_7'] = temp_data['depth'].rolling(window=7,center=False).mean()
+        temp_data['mag_avg_22'] = temp_data['mag'].rolling(window=22,center=False).mean() 
+        temp_data['mag_avg_15'] = temp_data['mag'].rolling(window=15,center=False).mean()
+        temp_data['mag_avg_7'] = temp_data['mag'].rolling(window=7,center=False).mean()
+        temp_data.loc[:, 'mag_outcome'] = temp_data.loc[:, 'mag_avg_7'].shift(days_out_to_predict * -1)
 
-        df_live.append(temp_df.tail(days_out_to_predict))
+        data_live.append(temp_data.tail(days_out_to_predict))
 
-        eq_data.append(temp_df)
+        eq_data.append(temp_data)
 
-    # concat all location-based dataframes into master dataframe
-    df = pd.concat(eq_data)
+    # Add together all location-based dataframes into master dataframe
+    
+    data = pd.concat(eq_data)
 
-    # remove any NaN fields
-    df = df[np.isfinite(df['depth_avg_22'])]
-    df = df[np.isfinite(df['mag_avg_22'])]
-    df = df[np.isfinite(df['mag_outcome'])]
+    # Remove all null fields
+    
+    data = data[np.isfinite(data['depth_avg_22'])]
+    data = data[np.isfinite(data['mag_avg_22'])]
+    data = data[np.isfinite(data['mag_outcome'])]
 
     # prepare outcome variable
-    df['mag_outcome'] = np.where(df['mag_outcome'] > 2.5, 1,0)
+    
+    data['mag_outcome'] = np.where(data['mag_outcome'] > 2.5, 1,0)
 
-    df = df[['date',
+    data = data[['date',
              'latitude',
              'longitude',
              'depth_avg_22',
@@ -89,16 +96,18 @@ def prepare_earthquake_data_and_model(days_out_to_predict = 5, max_depth=3, eta=
              'mag_outcome']]
 
     # keep only data where we can make predictions
-    df_live = pd.concat(df_live)
-    df_live = df_live[np.isfinite(df_live['mag_avg_22'])]
+    
+    data_live = pd.concat(data_live)
+    data_live = data_live[np.isfinite(data_live['mag_avg_22'])]
 
     # let's train the model whenever the webserver is restarted
+    
     from sklearn.model_selection import train_test_split
-    features = [f for f in list(df) if f not in ['date', 'mag_outcome', 'latitude',
+    features = [f for f in list(data) if f not in ['date', 'mag_outcome', 'latitude',
      'longitude']]
 
-    X_train, X_test, y_train, y_test = train_test_split(df[features],
-                         df['mag_outcome'], test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(data[features],
+                         data['mag_outcome'], test_size=0.2, random_state=42)
 
     dtrain = xgb.DMatrix(X_train[features], label=y_train)
     dtest = xgb.DMatrix(X_test[features], label=y_test)
@@ -111,52 +120,48 @@ def prepare_earthquake_data_and_model(days_out_to_predict = 5, max_depth=3, eta=
             'eta': eta,  # the training step for each iteration
             }  # logging mode - quiet}  # the number of classes that exist in this datset
 
-    num_round = 1000  # the number of training iterations    
+    # Determine the number of training iterations   
+    
+    num_round = 1000   
     early_stopping_rounds=30
     xgb_model = xgb.train(param, dtrain, num_round) 
 
 
     # train on live data
-    dlive = xgb.DMatrix(df_live[features])  
+    
+    dlive = xgb.DMatrix(data_live[features])  
     preds = xgb_model.predict(dlive)
 
-    # add preds to live data
-    df_live = df_live[['date', 'place', 'latitude', 'longitude']]
+    # add predictions to live data
+    
+    data_live = data_live[['date', 'place', 'latitude', 'longitude']]
+    
     # add predictions back to dataset 
-    df_live = df_live.assign(preds=pd.Series(preds).values)
+    
+    data_live = data_live.assign(preds=pd.Series(preds).values)
 
     # aggregate down dups
-    df_live = df_live.groupby(['date', 'place'], as_index=False).mean()
+    
+    data_live = data_live.groupby(['date', 'place'], as_index=False).mean()
 
     # increment date to include DAYS_OUT_TO_PREDICT
-    df_live['date']= pd.to_datetime(df_live['date'],format='%Y-%m-%d') 
-    df_live['date'] = df_live['date'] + pd.to_timedelta(days_out_to_predict,unit='d')
+    
+    data_live['date']= pd.to_datetime(data_live['date'],format='%Y-%m-%d') 
+    data_live['date'] = data_live['date'] + pd.to_timedelta(days_out_to_predict,unit='d')
 
-    return(df_live)
-
-def get_earth_quake_estimates(desired_date, df_live):
-
-# Description : gets desired date to predict earthquake and live prediction dataframe with NaN values as outcome magnitude 
-#probablity that has to be predicted. The function also deals with converting to google maps api format 
-#of location co-ordinates to mark it on the map.
-
-# Arguments : DateTime object (desired_date to predict), Pandas DataFrame (dataframe of prediction with NaN values as outcome)
-
-#Return : string (Google maps api format location coordinates)
-
-   
-    from datetime import datetime
-    live_set_tmp = df_live[df_live['date'] == desired_date]
-
+    return(data_live)
+    
+def get_earth_quake_estimates(desired_date, data_live):
+	from datetime import datetime
+	live_set_tmp = data_live[data_live['date'] == desired_date]
     # Convert to strings format lat/lon for Google Maps 
-    LatLngString = ''
-    if (len(live_set_tmp) > 0):
-        for lat, lon, pred in zip(live_set_tmp['latitude'], live_set_tmp['longitude'], live_set_tmp['preds']): 
-            # this is the threashold of probability to decide what to show and what not to show
-            if (pred > 0.3):
-                LatLngString += "new google.maps.LatLng(" + str(lat) + "," + str(lon) + "),"
-
-    return(LatLngString)
+	LatLngString = ''
+	if (len(live_set_tmp) > 0):
+		for lat, lon, pred in zip(live_set_tmp['latitude'], live_set_tmp['longitude'], live_set_tmp['preds']):
+			if (pred > 0.3):
+				LatLngString += "new google.maps.LatLng(" + str(lat) + "," + str(lon) + "),"
+		return(LatLngString)		 
+            
 
 
 @app.before_first_request
@@ -164,6 +169,7 @@ def startup():
     global earthquake_live
 
     # prepare data, model and get live data set with earthquake forecasts
+    
     earthquake_live = prepare_earthquake_data_and_model()
 
 
@@ -182,9 +188,11 @@ def build_page():
 
         else:
             # set blank map
+            
             return render_template('index.html',
                 date_horizon = datetime.today().strftime('%d/%m/%Y'),
                 earthquake_horizon = '',
                 current_value=0,
                 days_out_to_predict=days_out_to_predict)
-app.run(debug=True, port=80)
+#set debug mode true and run on port 88, for production purposes set localhost=0.0.0.0
+app.run(debug=True, port=90)
